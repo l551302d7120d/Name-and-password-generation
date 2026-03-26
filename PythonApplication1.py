a99667bd067402680e7fdfe403c37cb7f2081a56
l551302d7120d@outlook.com <l551302d7120d@outlook.com>
2026-03-26 22:57:17 +0800

    add pin

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -1,10 +1,13 @@
 import hashlib
+import re
 info = input("message:")
 sha512_hash = hashlib.sha512(info.encode()).hexdigest()
 print(sha512_hash)
 name_long = int(input("name_long:"))
 name = sha512_hash[0:name_long]
-print(name)
+print("name:"+name)
 key_long = int(input("key long:"))
 sha512_hash = sha512_hash[::-1]
-print(sha512_hash[0:key_long])
+print("key:"+sha512_hash[0:key_long])
+pin_sha512_hash = re.sub(r"\D", "", sha512_hash)
+print("pin:"+pin_sha512_hash[0:6])
